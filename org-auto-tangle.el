2982fc844954226f7d7b412a57a9d8dce509dc02
lispy-dobby <yilkalargawworkneh@gmail.com>
2021-02-07 10:06:43 +0000

    updated the commentary section

--- org-auto-tangle.el
+++ org-auto-tangle.el
@@ -34,9 +34,9 @@
 ;;; Commentary:
 
 ;; It is common to want to tangle org files everytime you save your changes,
-;; especially for common tangled init files. So this program allows you to
-;; do so using #+auto_tangle option in an org file. It also does so asynchronously
-;; so it does let you Emacs session hang
+;; especially for tangled init files. So this program allows you to
+;; do so using #+auto_tangle option in an org file. It also accomplishes 
+;; this feat asynchronously so it does not let you Emacs session hang.
 
 ;;; Usage:
 
