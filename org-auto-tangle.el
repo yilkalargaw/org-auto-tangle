c64ca020f99952cce9798209912a8e335900a1a4
lispy-dobby <yilkalargawworkneh@gmail.com>
2022-07-02 12:40:42 +0300

    update a major version number after a major refactor by seven monier

--- org-auto-tangle.el
+++ org-auto-tangle.el
@@ -2,7 +2,7 @@
 
 ;; Author: Yilkal Argaw <yilkalargawworkneh@gmail.com>
 ;; URL: https://github.com/yilkalargaw/org-auto-tangle
-;; Version: 0.4.2
+;; Version: 0.5.0
 ;; Keywords: outlines
 ;; Package-Requires: ((emacs "24.1") (async "1.9.3"))
 
